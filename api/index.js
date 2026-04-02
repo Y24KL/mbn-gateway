587b0b1a1587206eae4666cb24ef07c89c7258c0
Y24KL <gtrusouth116@gmail.com>
2026-04-02 16:34:54 +0100

    Replace decap-cms-oauth-vercel with netlify-cms-oauth-provider-node

--- api/index.js
+++ api/index.js
@@ -1,2 +1,6 @@
-const lib = require('decap-cms-oauth-vercel');
-module.exports = lib;
+const { createHandler } = require('netlify-cms-oauth-provider-node');
+
+module.exports = createHandler({
+  clientId: process.env.GITHUB_CLIENT_ID,
+  clientSecret: process.env.GITHUB_CLIENT_SECRET,
+});
